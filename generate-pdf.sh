33957e9a605770010e6f28490ff0368612c24bc9
ilmari-koria <ilmarikoria@posteo.net>
2025-04-20 18:36:06 +0100

    [MOD] add basic working sh script

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+CBETA_ID="$1"
+BASEX="./basex/bin/basex"
+PUBLISH_DIR="./publish"
+BXS="./src/bxs/generate-pdf.bxs"
+
+mkdir $PUBLISH_DIR
+
+$BASEX -bpublish-path=$PUBLISH_DIR \
+       -bcbeta-id="$1" \
+       -c $BXS
